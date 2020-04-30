c6e8b5d99598bc5bcc1254908f6c7e4dfc489170
Edgar Lozano <edgar@edgarl.com>
2020-04-30 16:47:56 -0500

    Added state change when clicking on numbered buttons. Operator buttons have some preliminary code, but logic is flawed.

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,9 +4,19 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input_str: '',
+      inputStr: '0',
       result: 0
     };
+  }
+  handleClear = () => {
+    this.setState({ inputStr: '0'});
+  }
+  handleClick = (e) => {
+    if(this.state.inputStr === '0') this.setState({ inputStr: e.target.value });
+    else this.setState({ inputStr: this.state.inputStr + e.target.value});
+  }
+  handleZero = () => {
+    if(this.state.inputStr !== '0') this.setState({ inputStr: this.state.inputStr + '0' });
   }
   render() {
     return (
@@ -15,29 +25,29 @@ class Calculator extends React.Component {
         <p>Buttons are grouped as numbers or operators. You can navigate to a button with the keyboard and press Enter to activate. The equals button will give you the result of your current calculation.</p>
         <h3>Numbers</h3>
         <div id="numbers">
-          <button id="one">1</button>
-          <button id="two">2</button>
-          <button id="three">3</button>
-          <button id="four">4</button>
-          <button id="five">5</button>
-          <button id="six">6</button>
-          <button id="seven">7</button>
-          <button id="eight">8</button>
-          <button id="nine">9</button>
-          <button id="zero">0</button>
+          <button value="1" onClick={this.handleClick} id="one">1</button>
+          <button value="2" onClick={this.handleClick} id="two">2</button>
+          <button value="3" onClick={this.handleClick} id="three">3</button>
+          <button value="4" onClick={this.handleClick} id="four">4</button>
+          <button value="5" onClick={this.handleClick} id="five">5</button>
+          <button value="6" onClick={this.handleClick} id="six">6</button>
+          <button value="7" onClick={this.handleClick} id="seven">7</button>
+          <button value="8" onClick={this.handleClick} id="eight">8</button>
+          <button value="9" onClick={this.handleClick} id="nine">9</button>
+          <button onClick={this.handleZero} id="zero">0</button>
         </div>
         <h3>Operators</h3>
         <div id="operators">
-          <button id="add">plus (+)</button>
-          <button id="subtract">minus (-)</button>
-          <button id="multiply">times (*)</button>
-          <button id="divide">divided by (/)</button>
+          <button value="+" onClick={this.handleClick} id="add">plus (+)</button>
+          <button value="-" onClick={this.handleClick} id="subtract">minus (-)</button>
+          <button value="*" onClick={this.handleClick} id="multiply">times (*)</button>
+          <button value="/" onClick={this.handleClick} id="divide">divided by (/)</button>
           <button id="decimal">Decimal point (.)</button>
           <button id="equals">equals (=)</button>
-          <button id="clear">Clear!</button>
+          <button onClick={this.handleClear} id="clear">Clear!</button>
         </div>
         <h3>Current Display</h3>
-        <div id="display"></div>
+        <div id="display">Display: {this.state.inputStr}</div>
       </div>
     );
   }
