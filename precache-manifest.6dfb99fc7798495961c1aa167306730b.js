22d42486c9375634b33cce4eefaca799f9fd3ac6
Edgar Lozano <edgar@edgarl.com>
2020-04-30 14:08:49 -0500

    Updates

---
+++ precache-manifest.6dfb99fc7798495961c1aa167306730b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7cb3b009c22c10cbc9829dbd44dad2",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "e3bb87d13776fb8ec3d1",
+    "url": "/react-calculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "af5a5cc785c45145056f",
+    "url": "/react-calculator/static/js/2.bf22b302.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.bf22b302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3bb87d13776fb8ec3d1",
+    "url": "/react-calculator/static/js/main.67a208d1.chunk.js"
+  },
+  {
+    "revision": "7a4e8bb32f6cfb799366",
+    "url": "/react-calculator/static/js/runtime-main.6252dc75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
