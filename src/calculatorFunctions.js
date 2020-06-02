d3057e3fa711b547bb3c139e88ca385271eb17f3
Edgar Lozano <edgar@edgarl.com>
2020-06-02 15:49:22 -0500

    Finished up calculation logic.

--- src/calculatorFunctions.js
+++ src/calculatorFunctions.js
@@ -12,3 +12,51 @@ function tokenize(str) {
   tokens.push(currentString);
   return tokens;
 }
+
+function reversePolishNotation(tokens) {
+  let queue = [];
+  let stack = [];
+  let precedence = {
+    "+": 1, "-": 1,
+    "/": 2, "*": 2
+  };
+  for(let token of tokens) {
+    let tokenPrecedence = precedence[token] || 0;
+    let expressionPrecedence = stack.length ? precedence[stack[stack.length-1]] : 0;
+    if(token.match(/[0-9]+/)) queue.push(token);
+    else {
+      while(tokenPrecedence <= expressionPrecedence) {
+        queue.push(stack.pop());
+        expressionPrecedence = stack.length ? precedence[stack[stack.length-1]] : 0;
+      }
+      stack.push(token);
+    }
+  }
+  for(let element of stack) {
+    queue.push(element);
+  }
+  return queue;
+}
+
+function calculate(q) {
+  let stack = [];
+  for(let token of q) {
+    if(token.match(/[0-9]+/)) {
+      stack.push(parseFloat(token));
+      continue;
+    }
+    let right = stack.pop();
+    let left = stack.pop();
+    switch(token) {
+      case '*': stack.push(left*right); break;
+      case '+': stack.push(left+right); break;
+      case '-': stack.push(left-right); break;
+      case '/': stack.push(left/right); break;
+    }
+  }
+  return stack.pop();
+}
+
+export default function getCalculation(calcString) {
+  return calculate(reversePolishNotation(tokenize(calcString)));
+}
